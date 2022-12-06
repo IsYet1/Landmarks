ee45aa09dfd82ceb37e486fb260548130164df29
donmck <donmck@amazon.com>
2022-12-05 18:32:06 -0600

    favorite button is working all the way through

---
+++ Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -0,0 +1,59 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Don McKenzie on 12/2/22.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
+    var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: {$0.id == landmark.id })!
+    }
+    
+    var body: some View {
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
+                .frame(height: 300.0)
+                .ignoresSafeArea(edges: .top
+                )
+            CircleImage(image: landmark.image)
+                .padding(.bottom, -130.0)
+                .offset(x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/-130.0/*@END_MENU_TOKEN@*/)
+            VStack(alignment: .leading) {
+                HStack {
+                    Text(landmark.name)
+                        .font(.title)
+                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                }
+                HStack {
+                    Text(landmark.park)
+                    Spacer()
+                    Text(landmark.state)
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                Divider()
+                Text("About \(landmark.name)")
+                    .font(.subheadline)
+                Text(landmark.description)
+            }
+            .padding()
+        }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static let modelData = ModelData()
+    
+    static var previews: some View {
+        LandmarkDetail(landmark: modelData.landmarks[0])
+            .environmentObject(modelData)
+    }
+}
