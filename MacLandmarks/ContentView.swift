56fd35de29d37ee6a08ad67c8d55182d22df5c27
donmck <donmck@amazon.com>
2023-01-21 16:22:48 -0600

    Mac app through section 2.

---
+++ MacLandmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by Don McKenzie on 21-01-2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
